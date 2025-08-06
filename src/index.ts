95e7f1e733e29fb3f800b6b35db5e86d3b27bc26
David Espinoza <despinozap.dev@gmail.com>
2025-08-06 14:26:47 -0300

    chore: Biome added as linter

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,38 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { z } from 'zod';
 
 // Create server
 const server = new McpServer({
-  name: 'My MCP Server',
-  version: '0.0.1',
+	name: 'My MCP Server',
+	version: '0.0.1',
 });
 
 // Define tools
 server.tool(
-  'echo_message',
-  'Echo a given message',
-  {
-    message: z.string(),
-  },
-  async ({ message }) => {
-    return {
-      content: [
-        {
-          type: 'text',
-          text: `The echo for the given message is ${message}`,
-        },
-      ]
-    };
-  },
+	'echo_message',
+	'Echo a given message',
+	{
+		message: z.string(),
+	},
+	async ({ message }) => {
+		return {
+			content: [
+				{
+					type: 'text',
+					text: `The echo for the given message is ${message}`,
+				},
+			],
+		};
+	},
 );
 
 // Listen client connections
 const transport = new StdioServerTransport();
 (async () => {
-  try {
-    await server.connect(transport);
-  }
-  catch (error) {
-    console.error(error);
-  }
+	try {
+		await server.connect(transport);
+	} catch (error) {
+		console.error(error);
+	}
 })();
